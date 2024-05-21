aa5d1cb06aa6559d20eb5bac41cc08df42c63e18
rango198 <137562811+rango198@users.noreply.github.com>
2024-05-21 14:24:42 +0300

    commit_one

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import './globals.css';
+import Providers from './components/providers';
+
+const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-xl">Home page</h1>
+
+    </main>
+  );
+}
